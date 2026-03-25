53056629aca989ea3e17f426b80bfe34ea76cf40
nlo <1>
2026-03-25 17:12:52 +0300

    Создание и чтение test.txt

--- lab11.php
+++ lab11.php
@@ -1,20 +1,9 @@
 <?php
-    $file=fopen("test.txt", "w");
-    fwrite($file, "Привет мир");
-    fclose($file);
-    $file = fopen("test.txt", "r");
-    while (!feof($file))
-{
-$str = fgets($file);
-echo $str."<br />";
+$file = fopen("test.txt", "w");
+fwrite($file, "Привет, мир!");
+fclose($file);
+$file = fopen("test.txt", "r");
+while (!feof($file)) {
+    echo fgets($file) . "<br>";
 }
-rename("test.txt", "mir.txt");
-mkdir("folder", 0700);
-rename("mir.txt", "/var/www/mySite.ru/folder");
-copy("mir.txt", "world.txt");
-echo filesize("world.txt");
-echo "<br>";
-echo filesize("world.txt") / 1024;
-echo "<br>";
-echo filesize("world.txt") / 1024 / 1024.<br>;
-?>
+fclose($file);
