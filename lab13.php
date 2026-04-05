7d91566e26eb76eb04ecc7e06c7aae0adb9bc5aa
nlo <1>
2026-04-05 20:34:55 +0300

    вывод зарплаты первого работника через созданный метод

--- lab13.php
+++ lab13.php
@@ -1,9 +1,9 @@
 <?php
 class BalukhWorker 
 {
-public $name;
-public $age;
-public $salary;
+private $name;
+private $age;
+private $salary;
 
 public function __construct($name, $age, $salary) {
 $this->name = $name;
@@ -11,10 +11,23 @@ $this->age = $age;
 $this->salary = $salary;
 
     }
+
+public function GetName() {
+return $this->name;
+    }
+
+public function GetAge() {
+return $this->age;    
+    }
+
+public function GetSalary() {
+return $this->salary;
+    }
 }
 
 $firstWorker = new BalukhWorker("Vasya", 22, 180000);
 $secondWorker = new BalukhWorker("Katya", 19, 60000);
 
-echo $firstWorker->salary + $secondWorker->salary . " - Сумма зарплат <br>";
-echo $firstWorker->age + $secondWorker->age . " - Сумма возрастов <br>";
+echo $firstWorker->GetSalary() . " - зарплата первого работника";
+
+
