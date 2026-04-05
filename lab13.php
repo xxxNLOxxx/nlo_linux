349525706735aec6b8fc9f37d921b212a7109ae8
nlo <1>
2026-04-05 20:59:53 +0300

    проверка возраста через новый метод

--- lab13.php
+++ lab13.php
@@ -16,11 +16,22 @@ public function GetName() {
 return $this->name;
     }
 
+public function GetAge() {
+return $this->age;
+    }
+
 public function SetAge($newAge) {
 if ($newAge > 17) { 
 return $this->age = $newAge;    
     }
 return "Вам еще рано работать в нашей компании";
+}
+
+public function CheckAge($age) {
+if ($age > 17) {
+return true;    
+    }
+return false;
 }
 
 public function GetSalary($otherWorker = null) {
@@ -38,4 +49,5 @@ $secondWorker = new BalukhWorker("Katya", 19, 60000);
 
 echo $firstWorker->GetSalary() . " - зарплата первого работника <br>";
 echo $firstWorker->GetSalary($secondWorker) . " - сумма зарплат работников <br>";
-echo $secondWorker->SetAge(16) . " - новый возраст пользователя <br>";
+echo $secondWorker->SetAge(20) . " - новый возраст пользователя <br>";
+echo $secondWorker->CheckAge($secondWorker->GetAge());
