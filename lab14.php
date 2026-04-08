1d5b74f63bf335b48988c51409a277ab96ea7d46
nlo <1>
2026-04-08 15:15:53 +0300

    Создаем новый класс и разметку для сайта

--- lab14.php
+++ lab14.php
@@ -9,4 +9,25 @@ class Page
         echo $this->template;
     }
 }
+
+class BlogPage extends Page 
+{
+    private string $name = 'blog';
+    private string $template = '
+        <div style="display: flex; gap: 15px;">
+            <div style="border: 1px solid #000; padding: 10px;">
+                <h3>Италия</h3>
+                <p>Рим и Колизей</p>
+            </div>
+            <div style="border: 1px solid #000; padding: 10px;">
+                <h3>Франция</h3>
+                <p>Париж и Лувр</p>
+            </div>
+        </div>';
+
+    public function render(): void 
+    {
+        echo $this->template;
+    }
+}
 ?>
