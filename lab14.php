11e7d6b0c47e512ec665f954e8dd035203f55c14
nlo <1>
2026-04-08 15:07:20 +0300

    Создали метод render

--- lab14.php
+++ lab14.php
@@ -3,5 +3,10 @@ class Page
 {
     private string $name = 'page';
     private string $template = '<div><p>It is a default page</p></div>';
+
+    public function render(): void
+    {
+        echo $this->template;
+    }
 }
 ?>
