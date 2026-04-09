e6ccf9a33c1c7e7f873a0a44c989d18f22d75ecb
nlo <1>
2026-04-09 15:39:06 +0300

    описываем классы

--- lab15.php
+++ lab15.php
@@ -36,24 +36,61 @@ public class Rectangle extends Figure implements Area {
 private $a;
 private $b;
 
-public function getArea() {}
-public function infoAbout() {}
+public function __construct($a, $b) {
+        $this->a = $a;
+        $this->b = $b;
+        $this->count_sides = 4;
+    }
+
+public function getArea() {
+    $this->area = $this->a * $this->b;
+        return $this->area;
+}
+
+public function infoAbout() {
+    return "Это прямоугольник. У него {$this->sideCount} стороны.";
+    }
 }
 
 public class Triangle extends Figure implements Area {
+
 private $a;
 private $b;
 private $c;
 
-public function getArea() {}
-public function infoAbout() {}
+ public function __construct($a, $b, $c) {
+        $this->a = $a;
+        $this->b = $b;
+        $this->c = $c;
+        $this->sideCount = 3;
+    }
+
+public function getArea() {
+$p = ($this->a + $this->b + $this->c) / 2; 
+        $this->area = sqrt($p * ($p - $this->a) * ($p - $this->b) * ($p - $this->c));
+        return $this->area;
+}
+
+public function infoAbout() {
+return "Это треугольник. У него {$this->sideCount} стороны."
+    }
 }
 
 public class Square extends Figure implements Area {
 private $a;
 
-public function getArea() {}
-public function infoAbout() {}
+public function __construct($a) {
+        $this->a = $a;
+        $this->sideCount = 4;
+    }
+
+public function getArea() {
+    $this->area = pow($this->a, 2);
+        return $this->area;
+}
+public function infoAbout() {
+    return "Это квадрат. У него {$this->sideCount} стороны.";
+    }
 }
 
 
