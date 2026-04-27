d36e0ac1bfe2ae7eca259aafd7c53b6aa9d50672
nlo <1>
2026-04-27 15:22:03 +0300

    Обновили форму контакта

--- pages/contact.php
+++ pages/contact.php
@@ -1,12 +1,23 @@
-<?php include '../includes/header.php'; ?>
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name'] ?? '');
+    $email = htmlspecialchars($_POST['email'] ?? '');
+    $res = "Спасибо, $name! Сообщение получено.";
+}
+include '../includes/header.php';
+?>
+
 <main>
     <h1>Контакты</h1>
+    <?php if (isset($res)): ?>
+        <p style="color: green;"><?= $res ?></p>
+    <?php endif; ?>
     <form method="post">
-        <input type="text" name="name" placeholder="Имя">
-        <input type="email" name="email" placeholder="Email">
-        <textarea name="message"></textarea>
+        <input type="text" name="name" placeholder="Имя" required><br><br>
+        <input type="email" name="email" placeholder="Email" required><br><br>
+        <textarea name="message" placeholder="Сообщение"></textarea><br><br>
         <button type="submit">Отправить</button>
     </form>
 </main>
-<?php include '../includes/footer.php'; ?>
 
+<?php include '../includes/footer.php'; ?>
